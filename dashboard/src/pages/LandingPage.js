c0501a67853310fcdc27efb15d120f4154b58229
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-08-13 16:42:43 +0530

    W

--- dashboard/src/pages/LandingPage.js
+++ dashboard/src/pages/LandingPage.js
@@ -1,44 +1,19 @@
-// material
-import { experimentalStyled as styled } from "@material-ui/core/styles";
-// components
-import Page from "../components/Page";
-import {
-  LandingHero,
-  LandingMinimal,
-  LandingDarkMode,
-  LandingAdvertisement,
-  LandingCleanInterfaces,
-  LandingHugePackElements,
-} from "../components/_external-pages/landing";
+import React from 'react';
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled(Page)({
-  height: "100%",
-});
 
-const ContentStyle = styled("div")(({ theme }) => ({
-  overflow: "hidden",
-  position: "relative",
-  backgroundColor: theme.palette.background.default,
-}));
+
+const redirect = () => {
+  window.location.href = "https://saas-listapp-dashboard.vercel.app/auth/login-unprotected";
+}
 
 // ----------------------------------------------------------------------
 
 export default function LandingPage() {
   return (
-    <RootStyle
-      title="The starting point for your next project | List App"
-      id="move_top"
-    >
-      <LandingHero />
-      <ContentStyle>
-        <LandingMinimal />
-        <LandingHugePackElements />
-        <LandingDarkMode />
-        <LandingCleanInterfaces />
-        <LandingAdvertisement />
-      </ContentStyle>
-    </RootStyle>
+    <>
+     {redirect()}
+    </>
   );
 }
