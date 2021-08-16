3adbbc14e17ae073712655e43777ba83ceb2258e
Manav Nanwani <manav.nanwani@gmail.com>
2021-08-16 10:58:45 +0530

    update

--- dashboard/src/pages/LandingPage.js
+++ dashboard/src/pages/LandingPage.js
@@ -3,8 +3,9 @@ import React from "react";
 // ----------------------------------------------------------------------
 
 const redirect = () => {
-  // window.location.href = "https://saas-listapp-dashboard.vercel.app/auth/login-unprotected";
+  window.location.href = "https://saas-listapp-dashboard.vercel.app/auth/login-unprotected";
 };
+
 
 // ----------------------------------------------------------------------
 
