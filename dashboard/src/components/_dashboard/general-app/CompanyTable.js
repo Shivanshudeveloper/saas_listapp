686f2fcaabd196e883a3c5203c2b07af5e1fb87d
Manav Nanwani <manav.nanwani@gmail.com>
2021-08-31 15:22:15 +0530

    Updates

--- dashboard/src/components/_dashboard/general-app/CompanyTable.js
+++ dashboard/src/components/_dashboard/general-app/CompanyTable.js
@@ -10,12 +10,23 @@ import {
   TableBody,
   TableCell,
   TableHead,
-  CardHeader,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  Container,
+  TextField,
   Typography,
   TableContainer,
+  Grid,
 } from "@material-ui/core";
 import FilterListIcon from "@material-ui/icons/FilterList";
+import { DropzoneArea } from "material-ui-dropzone";
 import Scrollbar from "../../Scrollbar";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 import LockIcon from "@material-ui/icons/Lock";
 import SearchIcon from "@material-ui/icons/Search";
@@ -81,25 +92,102 @@ export default function CompanyTable({ handleClickOpen }) {
     setAnchorEl(null);
   };
 
+  const GridCompany = ({ label }) => {
+    return (
+      <Grid item md={6}>
+        <TextField
+          style={{ marginTop: "5px" }}
+          label={label}
+          variant="filled"
+          size="small"
+          fullWidth
+        />
+      </Grid>
+    );
+  };
+  const [open, setOpen] = useState(false);
+  const handleClickOpenDialog = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+
   const tableCellStyle = { paddingTop: "5px", paddingBottom: "5px" };
 
   return (
     <Card>
+      <Dialog fullScreen open={open} onClose={handleCloseDialog} fullWidth>
+        <DialogTitle id="form-dialog-title">
+          <Container maxWidth="md">Add Company</Container>
+        </DialogTitle>
+        <DialogContent>
+          <br />
+          <Container maxWidth="md">
+            <Grid container spacing={2}>
+              <GridCompany label="Website" />
+              <GridCompany label="Name" />
+              <GridCompany label="Facebook" />
+              <GridCompany label="Twitter" />
+              <GridCompany label="Phone" />
+              <GridCompany label="Industry" />
+              <GridCompany label="About" />
+              <GridCompany label="Street Address" />
+              <GridCompany label="City" />
+              <GridCompany label="State" />
+              <GridCompany label="Country" />
+              <GridCompany label="Postal Code" />
+              <GridCompany label="Linkedin" />
+              <GridCompany label="Number of Employees" />
+              <Grid item md={12}>
+                <DropzoneArea
+                  onChange={(files) => console.log("Files:", files)}
+                />
+              </Grid>
+            </Grid>
+          </Container>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleCloseDialog}
+            color="primary"
+            variant="contained"
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
       <div
         style={{
           padding: "13px",
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <Typography variant="h6">All Companies</Typography>
-        <Button
-          variant="outlined"
-          onClick={handleClickOpen}
-          startIcon={<FilterListIcon />}
-          style={{ marginLeft: "20px" }}
+        <div
+          style={{
+            padding: "13px",
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Filter
+          <Typography variant="h6">All Companies</Typography>
+          <Button
+            variant="outlined"
+            onClick={handleClickOpen}
+            startIcon={<FilterListIcon />}
+            style={{ marginLeft: "20px" }}
+          >
+            Filter
+          </Button>
+        </div>
+        <Button variant="contained" onClick={handleClickOpenDialog}>
+          Add Company
         </Button>
       </div>
 
--- dashboard/src/components/_dashboard/general-app/ContactTable.js
+++ dashboard/src/components/_dashboard/general-app/ContactTable.js
@@ -16,13 +16,18 @@ import {
   TableBody,
   TableCell,
   TableHead,
-  CardHeader,
+  TextField,
   TableContainer,
   IconButton,
   Menu,
   MenuItem,
   Avatar,
   Typography,
+  Grid,
+  InputLabel,
+  FormControl,
+  Select,
+  Container,
 } from "@material-ui/core";
 import Scrollbar from "../../Scrollbar";
 
@@ -30,6 +35,10 @@ import LockIcon from "@material-ui/icons/Lock";
 
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import FilterListIcon from "@material-ui/icons/FilterList";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 // ----------------------------------------------------------------------
 
 const INVOICES = [
@@ -90,26 +99,150 @@ export default function ContactTable({ handleClickOpen }) {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpenDialog = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
 
   const tableCellStyle = { paddingTop: "5px", paddingBottom: "5px" };
 
+  const GridContact = ({ label }) => {
+    return (
+      <Grid item md={6}>
+        <TextField
+          style={{ marginTop: "5px" }}
+          label={label}
+          variant="filled"
+          size="small"
+          fullWidth
+        />
+      </Grid>
+    );
+  };
+
   return (
     <Card>
+      <Dialog fullScreen open={open} onClose={handleCloseDialog} fullWidth>
+        <DialogTitle id="form-dialog-title">
+          <Container maxWidth="md">Add Contact</Container>
+        </DialogTitle>
+        <DialogContent>
+          <br />
+          <Container maxWidth="md">
+            <Grid container spacing={2}>
+              <GridContact label="First Name" />
+              <GridContact label="Last Name" />
+              <GridContact label="Company" />
+              <GridContact label="Email" />
+              <Grid item md={6}>
+                <FormControl
+                  fullWidth
+                  variant="filled"
+                  style={{ marginTop: "5px" }}
+                >
+                  <InputLabel>Email Type</InputLabel>
+                  <Select fullWidth size="small">
+                    <MenuItem value={1}>Work</MenuItem>
+                    <MenuItem value={2}>Personal</MenuItem>
+                    <MenuItem value={3}>Other</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <GridContact label="Title" />
+              <GridContact label="Phone" />
+              <Grid item md={6}>
+                <FormControl
+                  fullWidth
+                  variant="filled"
+                  style={{ marginTop: "5px" }}
+                >
+                  <InputLabel>Phone Type</InputLabel>
+                  <Select fullWidth size="small">
+                    <MenuItem value={1}>Work</MenuItem>
+                    <MenuItem value={2}>Home</MenuItem>
+                    <MenuItem value={3}>Mobile</MenuItem>
+                    <MenuItem value={4}>Other</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <GridContact label="Extn" />
+              <Grid item md={6}>
+                <FormControl
+                  fullWidth
+                  variant="filled"
+                  style={{ marginTop: "5px" }}
+                >
+                  <InputLabel>Stage</InputLabel>
+                  <Select fullWidth size="small">
+                    <MenuItem value={1}>New</MenuItem>
+                    <MenuItem value={2}>Cold</MenuItem>
+                    <MenuItem value={3}>Bad Contact Info</MenuItem>
+                    <MenuItem value={4}>Replied</MenuItem>
+                    <MenuItem value={5}>Unresponsive</MenuItem>
+                    <MenuItem value={6}>Interested</MenuItem>
+                    <MenuItem value={7}>Not Interested</MenuItem>
+                    <MenuItem value={8}>Do Not Contact</MenuItem>
+                    <MenuItem value={9}>Opt-Out</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
+              <GridContact label="Street Address" />
+              <GridContact label="City" />
+              <GridContact label="State/Region" />
+              <GridContact label="Country" />
+              <GridContact label="Postal Code" />
+              <GridContact label="Linkedin" />
+              <GridContact label="Facebook" />
+              <GridContact label="Contact Owner (Users from the System)" />
+              <GridContact label="Tags" />
+            </Grid>
+          </Container>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={handleCloseDialog}
+            color="primary"
+            variant="contained"
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
       <div
         style={{
           padding: "13px",
           display: "flex",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <Typography variant="h6">All Contacts</Typography>
-        <Button
-          variant="outlined"
-          onClick={handleClickOpen}
-          startIcon={<FilterListIcon />}
-          style={{ marginLeft: "20px" }}
+        <div
+          style={{
+            padding: "13px",
+            display: "flex",
+            alignItems: "center",
+          }}
         >
-          Filter
+          <Typography variant="h6">All Contacts</Typography>
+          <Button
+            variant="outlined"
+            onClick={handleClickOpen}
+            startIcon={<FilterListIcon />}
+            style={{ marginLeft: "20px" }}
+          >
+            Filter
+          </Button>
+        </div>
+        <Button variant="contained" onClick={handleClickOpenDialog}>
+          Add Contact
         </Button>
       </div>
       <Scrollbar>
