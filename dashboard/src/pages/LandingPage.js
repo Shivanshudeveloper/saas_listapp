42bd60331141ee81d0e71231dd5b35b7c8e74695
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-09-04 09:29:04 +0530

    S

--- dashboard/src/pages/LandingPage.js
+++ dashboard/src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import React from "react";
 // ----------------------------------------------------------------------
 
 const redirect = () => {
-  // window.location.href = "https://saas-listapp-dashboard.vercel.app/auth/login-unprotected";
+  window.location.href = "https://saas-listapp-dashboard.vercel.app/auth/login-unprotected";
 };
 
 // ----------------------------------------------------------------------
