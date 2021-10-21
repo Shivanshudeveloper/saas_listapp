de2b7d39afd033e20d2a0adb82271aff26931535
nandhamurali <45518966+nandhamurali@users.noreply.github.com>
2021-10-21 14:40:23 -0700

    Update keys.js

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://adminuser:testing@1234@cluster0.awoki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://adminuser:testing1234@cluster0.awoki.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
 
 // module.exports = {
